607c6651135b2737b09544ec1ca43f2d1f1e1af0
Zeo <leonardo.basso02@gmail.com>
2022-04-06 18:54:59 +0200

    il compilatore mi da degli errori, ma in cuor mio so che questa cosa mia potrebbe essere giusta

--- array_pari_ricorsivo.c
+++ array_pari_ricorsivo.c
@@ -9,22 +9,26 @@ int main() {
     // scrivere una sorta di array di prova
     
     int array[] = {2, 4, 6, 8, 10};
-    int dimensione = 5;
-    
-    int risultato = array_pari(array, dimensione - 1);
-    if (risultato) printf("l'array contiene solo elementi pari"); 
-    else printf("l'array contiene almento un elemento dispari"); 
+    int dimensione = sizeof(array) / sizeof(int);
+    int risultato = array_pari(array + dimensione - 1,  dimensione);
+    printf("%d", risultato);
+    if (risultato == 1) 
+        printf("l'array contiene solo elementi pari"); 
+    else 
+        printf("l'array contiene almento un elemento dispari"); 
 }
 
 int array_pari(int *X, int dim) {
     /* PRE: dim è un numero strettamente maggiore di 0
      * POST: ritorna 1 se ogni elemento dell'array è un numero pari, altrimenti ritorna 0
      */ 
-    if (dim ==0) return 1;
-    else {
-        if (X[dim]%2 != 0)
-            return 0;
-        array_pari(X, dim - 1);
+    if (dim == 0)  {
+        printf("array passato nella sua totalità\n");
+        return 1;
     }
+    if (*X % 2 != 0)
+        return 0;
+    printf("%d, %d\n", dim, *X);
+    array_pari(X - 1, dim - 1);
 }
 
