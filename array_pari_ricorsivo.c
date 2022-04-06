285e3adb79b93550ea4c6e7b030053b4878f56fa
Zeo <leonardo.basso02@gmail.com>
2022-04-06 18:13:11 +0200

    modifiche aggiunte

--- array_pari_ricorsivo.c
+++ array_pari_ricorsivo.c
@@ -8,6 +8,12 @@ int array_pari(int *X, int dim);
 int main() {
     // scrivere una sorta di array di prova
     
+    int array[] = {2, 4, 6, 8, 10};
+    int dimensione = 5;
+    
+    int risultato = array_pari(array, dimensione - 1);
+    if (risultato) printf("l'array contiene solo elementi pari"); 
+    else printf("l'array contiene almento un elemento dispari"); 
 }
 
 int array_pari(int *X, int dim) {
